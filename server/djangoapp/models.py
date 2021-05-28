55d0038367f648e6ffe96aecee132f7bddc56129
RMG <rmgraves@gmail.com>
2021-05-28 06:41:09 +0000

    added cloudant integration

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -15,13 +15,6 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-class CarDealer(models.Model):
-    name = models.TextField()
-
-    def __str__(self):
-        return self.name
-
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
@@ -41,13 +34,37 @@ class CarModel(models.Model):
     ]
     make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.TextField()
-    dealer_id = models.ForeignKey(CarDealer, on_delete=models.CASCADE)
+    dealer_id = ''
     type = models.CharField(max_length=5, choices=CAR_TYPES, default=SUV)
     year = models.DateField(default=now)
     
     def __str__(self):
         return self.name
 
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer(models.Model):
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
